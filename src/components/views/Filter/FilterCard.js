7b7bd6fcc8a65a53ed1f7ae0e588c8e0208d4aa7
hyo814 <ggamjige8888@naver.com>
2021-03-14 22:02:05 +0900

    Filter

---
+++ src/components/views/Filter/FilterCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const tablestyle = {
+    lineHeight: "20px"
+}
+
+class FilterCard extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
+    render() {
+        return (
+            <tr>
+                <td style={tablestyle}>
+                    {this.props.person_id}
+                </td>
+                <td style={tablestyle}>
+                    {this.props.gender_source_value}
+                </td >
+                <td style={tablestyle}>
+                    {this.props.birth_datetime}
+                </td>
+                <td style={tablestyle}>
+                    {this.props.race_source_value}
+                </td>
+                <td style={tablestyle}>
+                    {this.props.ethnicity_source_value}
+                </td>
+                <td style={tablestyle}>
+                    2021 - {this.props.birth_datetime.substring(0,4)}
+                </td>
+            </tr>
+        );
+    }
+}
+export default FilterCard;
